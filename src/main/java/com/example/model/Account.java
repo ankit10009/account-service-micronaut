2f52c84248492f2f765a75ae3f83656d346097f2
SoumickSaha <ssaha2009@gmail.com>
2024-10-02 19:24:07 -0500

    Entity updated without lombok

--- src/main/java/com/example/model/Account.java
+++ src/main/java/com/example/model/Account.java
@@ -1,9 +1,6 @@
 package com.example.model;
 
-import io.micronaut.core.annotation.Introspected;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import java.math.BigDecimal;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,23 +8,63 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import java.math.BigDecimal;
+import io.micronaut.core.annotation.Introspected;
 
 @Introspected
 @Entity
 @Table(name = "accounts")
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Account {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    private String accountNumber;
-    private BigDecimal balance;
-    private String status;
+	private String accountNumber;
+	private BigDecimal balance;
+	private String status;
 
-    
+	public Account() {
+		super();
+	}
+
+	public Account(Long id, String accountNumber, BigDecimal balance, String status) {
+		super();
+		this.id = id;
+		this.accountNumber = accountNumber;
+		this.balance = balance;
+		this.status = status;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+
+	public void setAccountNumber(String accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+
+	public BigDecimal getBalance() {
+		return balance;
+	}
+
+	public void setBalance(BigDecimal balance) {
+		this.balance = balance;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
 }
